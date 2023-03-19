aeb6e672766850ae2ff2d39fc4aa91dbbb6df883
YoussefHossamEldein <youssefhossameldeinn@gmail.com>
2023-03-19 14:07:45 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 57; n >= 48; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
