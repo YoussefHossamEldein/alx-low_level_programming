291ec92f8c028efacd2dca9bae5e7b91871fc62d
YoussefHossamEldein <youssefhossameldeinn@gmail.com>
2023-03-24 16:21:08 -0700

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_diagonal - functiont that draws a diagonal
+ * Return: void
+ * @n: no of times
+ *
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
